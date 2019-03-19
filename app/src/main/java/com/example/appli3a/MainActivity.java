1a6fb0098d8bfdeb1221bdd4b84081be0854ca4b
AlexSarah <sarah.alexandre14@gmail.com>
2019-03-19 23:06:11 +0100

    Second Activity

--- app/src/main/java/com/example/appli3a/MainActivity.java
+++ app/src/main/java/com/example/appli3a/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.appli3a;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mMessageEditText;
+    public static final String EXTRA_MESSAGE =
+            "com.example.android.appli3a.extra.MESSAGE";
 
     private static final String LOG_TAG =
             MainActivity.class.getSimpleName();
@@ -14,11 +20,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMessageEditText = (EditText) findViewById(R.id.editText_main);
     }
 
     public void launchSecondActivity(View view) {
 
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        String message = mMessageEditText.getText().toString();
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+
         Log.d(LOG_TAG, "Button clicked!");
 
+        startActivity(intent);
+
     }
+
 }
+
+
