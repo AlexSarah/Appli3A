4619908eca6df749790ac2392b16ccc3650b3782
AlexSarah <sarah.alexandre14@gmail.com>
2019-03-25 20:06:59 +0100

    API + Second Activity

--- app/src/main/java/com/example/appli3a/MainActivity.java
+++ app/src/main/java/com/example/appli3a/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.appli3a;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 import java.util.List;
 
@@ -24,13 +26,15 @@ public class MainActivity extends Activity {
         controller = new MainController(this);
         controller.onStart();
 
+        //mMessageEditText = (EditText) findViewById(R.id.editText_main);
+
         // use this setting to
         // improve performance if you know that changes
         // in content do not change the layout size
         // of the RecyclerView
     }
 
-    public void showList(List<Api> input){
+    public void showList(final List<Api> input){
         recyclerView.setHasFixedSize(true);
         // use a linear layout manager
         layoutManager = new LinearLayoutManager(this);
@@ -38,5 +42,38 @@ public class MainActivity extends Activity {
         // define an adapter
         mAdapter = new MyAdapter(input);
         recyclerView.setAdapter(mAdapter);
+        recyclerView.addOnItemTouchListener(new Click(getApplicationContext(), recyclerView, new Click.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Api api= input.get(position);
+                //Toast.makeText(getApplicationContext(), ((Api) api).getName(), Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtra("description",api.getDescription());
+                intent.putExtra("name",api.getName());
+                startActivity(intent);
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+
+    }
+
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+    //public static final String EXTRA_MESSAGE ="com.example.appli3a.extra.MESSAGE";
+
+    //private EditText mMessageEditText;
+
+    public void launchSecondActivity(View view) {
+        //Log.d(LOG_TAG, "Button clicked!");
+
+        Intent intent = new Intent(this, SecondActivity.class);
+        //String message = mMessageEditText.getText().toString();
+
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/appli3a/SecondActivity.java
+++ app/src/main/java/com/example/appli3a/SecondActivity.java
@@ -1,7 +1,9 @@
 package com.example.appli3a;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        TextView name = findViewById(R.id.name);
+        TextView description = findViewById(R.id.description);
+
+        Intent intent = getIntent();
+        name.setText(intent.getStringExtra("name"));
+        description.setText((intent.getStringExtra("description")));
     }
+
+
 }
